d27445dbd9de5c2760e19d778eea33462c4cce87
megastruktur <astromortis@gmail.com>
2021-05-08 11:55:12 +0300

    Restock reagents with Necro

---
+++ organizer_restock_reagents.py
@@ -0,0 +1,72 @@
+# Adjust to whether to not you want to remove reagents from your bag if you have more than the desired amount
+removeReagentsIfOverRestockToAmount = True
+
+houseReagentsChestSerial = 0x43923CEA
+houseTopLeftCornerX = 975
+houseTopLeftCornerY = 452
+houseBottomRightCornerX = 987
+houseBottomRightCornerY = 469
+
+import Misc, Items, Player, Target
+from Scripts.utilities.items import FindItem, MoveItem, FindNumberOfItems
+from Scripts.glossary.items.reagents import reagents, reagentsNecro
+from Scripts.glossary.colors import colors
+from Scripts import config
+
+# Usage example: RestockReagents(50, 'necro')
+def RestockReagents(restockTo, type = 'magery'):
+    global removeReagentsIfOverRestockToAmount
+
+    reagentsBagSharedValue = 'reagentsBag'
+
+    if not Misc.CheckSharedValue( reagentsBagSharedValue ):
+        reagentsBag = Target.PromptTarget( 'Select bag to move the reagents into' )
+        Misc.SetSharedValue( reagentsBagSharedValue, reagentsBag )
+
+    reagentsBag = Misc.ReadSharedValue( reagentsBagSharedValue )
+    if reagentsBag == None or Items.FindBySerial( reagentsBag ) == None:
+        Player.HeadMessage( colors[ 'red' ], 'Can\'t find reagents bag! Clearing stored bag, please run again' )
+        Misc.RemoveSharedValue( reagentsBagSharedValue )
+        return
+
+    reagentsBag = Items.FindBySerial( reagentsBag )
+
+    if ( Player.Position.X >= houseTopLeftCornerX and Player.Position.Y >= houseTopLeftCornerY and
+            Player.Position.X <= houseBottomRightCornerX and Player.Position.Y <= houseBottomRightCornerY ):
+        reagentsSource = Items.FindBySerial( houseReagentsChestSerial )
+    else:
+        reagentsSource = Target.PromptTarget( 'Select container to restock from' )
+        reagentsSource = Items.FindBySerial( reagentsSource )
+        if reagentsSource == None or not reagentsSource.IsContainer:
+            Player.HeadMessage( colors[ 'red' ], 'Invalid source container' )
+
+    Items.UseItem( reagentsSource )
+    Misc.Pause( config.dragDelayMilliseconds )
+
+    # Switch reagent type
+    if type == 'necro':
+        regs = reagentsNecro
+    else:
+        regs = reagents
+    
+    reagentItemIDs = [ regs[ reagent ].itemID for reagent in regs ]
+    reagentsInBag = FindNumberOfItems( reagentItemIDs, reagentsBag )
+    
+    for reagent in regs:
+        Misc.Pause(1000)
+        currentReagent = regs[ reagent ]
+        if reagentsInBag[ currentReagent.itemID ] == restockTo:
+            continue
+        elif reagentsInBag[ currentReagent.itemID ] < restockTo:
+            reagentStackFromReagentsBag = FindItem( currentReagent.itemID, reagentsBag )
+            reagentStackFromSourceContainer = FindItem( currentReagent.itemID, reagentsSource )
+            if reagentStackFromReagentsBag == None:
+                MoveItem( Items, Misc, reagentStackFromSourceContainer, reagentsBag, restockTo )
+            else:
+                MoveItem( Items, Misc, reagentStackFromSourceContainer, reagentsBag, restockTo - reagentStackFromReagentsBag.Amount )
+        elif removeReagentsIfOverRestockToAmount:
+            reagentStackFromReagentsBag = FindItem( currentReagent.itemID, reagentsBag )
+            Misc.SendMessage( 'Removing %i %s' % ( reagentStackFromReagentsBag.Amount - restockTo, currentReagent.name ) )
+            MoveItem( Items, Misc, reagentStackFromReagentsBag, reagentsSource, reagentStackFromReagentsBag.Amount - restockTo )
+            
+    Misc.SendMessage('Done with ' + type, colors[ 'green' ])
